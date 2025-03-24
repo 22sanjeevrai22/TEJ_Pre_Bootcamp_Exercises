efb4144b45b99a33a382941c3f2f3f97376f9888
Sanjeev Rai <sunzeevraee@gmail.com>
2025-03-24 14:55:42 +0545

    Coercion custom exercises uploaded

---
+++ myCustomExercises/coercion.js
@@ -0,0 +1,173 @@
+//Some exercises from the ChatGPT
+
+1. What is the result of the following?
+false == "0"
+Answer: true
+
+2. What is the result of the following?
+null == undefined
+Answer: true
+
+3. What is the result of the following?
+0 == ""
+Answer: true
+
+4. What is the result of the following?
+0 == "0"
+Answer: true
+
+5. What is the result of the following?
+[] == false
+Answer: true
+
+6. What is the result of the following?
+true == "1"
+Answer: true
+
+7. What is the result of the following?
+NaN == NaN
+Answer: false
+
+8. What is the result of the following?
+[1] == true
+Answer: true
+
+9. What is the result of the following?
+5 + "3"
+Answer: "53"
+
+10. What is the result of the following?
+true + "3"
+Answer: "13"
+
+11. What is the result of the following?
+null + "hello"
+Answer: "nullhello"
+
+12. What is the result of the following?
+undefined + "world"
+Answer: "undefinedworld"
+
+13. What is the result of the following?
+[] + 2
+Answer: "2"
+
+14. What is the result of the following?
+[1,2] + 3
+Answer: "1,23"
+
+15. What is the result of the following?
+{} + []
+Answer: [object Object] (in newer browsers), 0 (in older browsers)
+
+16. What is the result of the following?
+5 + {}
+Answer: "5[object Object]"
+
+17. What is the result of the following?
+"6" - "2"
+Answer: 4
+
+18. What is the result of the following?
+true - 1
+Answer: 0
+
+19. What is the result of the following?
+null * 4
+Answer: 0
+
+20. What is the result of the following?
+{} + []
+Answer: [object Object]
+
+//Next 30 questions 
+
+1. {} == "0" 
+Answer: true
+
+2. null == "null" 
+Answer: false
+
+3. undefined == 0 
+Answer: false
+
+4. false + {} 
+Answer: '0[object Object]'
+
+5. {} + 1 
+Answer: '[object Object]1' (modern browsers), 1 (older browsers)
+
+6. true + "4" 
+Answer: '15'
+
+7. [] == ![] 
+Answer: true
+
+8. [] == true 
+Answer: false
+
+9. ![] == 0 
+Answer: false
+
+10. +{} 
+Answer: NaN
+
+11. [] + [] 
+Answer: ' '
+
+12. null + true 
+Answer: 1
+
+13. "" == "0" 
+Answer: false
+
+14. "0" - 0 
+Answer: 0
+
+15. {} - [] 
+Answer: 0 (older browsers) or NaN (modern browsers)
+
+16. false == "false" 
+Answer: false
+
+17. null == false 
+Answer: false
+
+18. "" == "0" 
+Answer: false
+
+19. "0" == 0 
+Answer: true
+
+20. false + undefined 
+Answer: NaN
+
+21. " " + 0 
+Answer: ' 0'
+
+22. false + "true" 
+Answer: 'falsetrue'
+
+23. {} + [] 
+Answer: '[object Object]' (newer browsers), 0 (older browsers)
+
+24. +null 
+Answer: 0
+
+25. 1 + "1" 
+Answer: '11'
+
+26. [] == [] 
+Answer: false
+
+27. "1" == true 
+Answer: true
+
+28. null == 0 
+Answer: false
+
+29. NaN == NaN 
+Answer: false
+
+30. {} == true 
+Answer: false
