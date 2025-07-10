2870d0d7110fe0039520aed4b06ce08d5f79092e
Sanjeev Rai <87857906+22sanjeevrai22@users.noreply.github.com>
2025-07-10 18:02:49 +0545

    Update remove-columns.js
    Added good method

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,15 +1,33 @@
 // YOUR CODE BELOW
 //My method(It works)
+// const removeColumns = (originalGrid, removeColumn) => {
+//   let newArray = [];
+//   for (let i = 0; i < originalGrid.length; i++) {
+//     newArray[i] = [];
+//     for (let j = 0; j < originalGrid[i].length - removeColumn; j++) {
+//       newArray[i][j] = originalGrid[i][j];
+//     }
+//   }
+//   return newArray;
+// };
+
 const removeColumns = (originalGrid, removeColumn) => {
-  let newArray = [];
+  const newArray = [];
+
   for (let i = 0; i < originalGrid.length; i++) {
-    newArray[i] = [];
-    for (let j = 0; j < originalGrid[i].length - removeColumn; j++) {
-      newArray[i][j] = originalGrid[i][j];
+    const newRow = [];
+    const newLength = originalGrid[i].length - removeColumn;
+
+    for (let j = 0; j < newLength; j++) {
+      newRow.push(originalGrid[i][j]);
     }
+
+    newArray.push(newRow);
   }
+
   return newArray;
 };
+
 
 //From Hints
 const removeColumns2 = (originalGrid, removeColumns) => {
