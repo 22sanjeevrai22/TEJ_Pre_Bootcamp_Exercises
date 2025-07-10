4f442c50e48d1a4e1166cae0e86c5c4f77b47d95
Sanjeev Rai <87857906+22sanjeevrai22@users.noreply.github.com>
2025-07-10 07:50:51 +0545

    Update zero-dark-thirty.js
    Updated new answer

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -6,3 +6,15 @@ const zeroDarkThirty = (num) => {
     return strippedNumber;
   }
 };
+
+function zeroDarkThirty2(a) {
+  let result = 1;
+  if (a === 0) return NaN;
+  for (i = 0; i < a.lenght; i++) {
+    if (a[i] === 0) {
+      continue;
+      result += 0;
+    }
+  }
+  return result;
+}
