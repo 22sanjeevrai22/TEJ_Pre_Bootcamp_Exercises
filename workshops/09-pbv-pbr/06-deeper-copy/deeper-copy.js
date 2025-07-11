83118b9b3aae5b4c7b0bda2e01202ec1e99cd043
Sanjeev Rai <87857906+22sanjeevrai22@users.noreply.github.com>
2025-07-11 15:01:39 +0545

    Update deeper-copy.js
    Hello deep-copy optimised

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -6,12 +6,33 @@ function deeperCopy(nestedArr) {
     if (typeof element === "object") {
       let insideNewArr = [];
       for (let j = 0; j < element.length; j++) {
-        newArr.push(element[i]);
+        insideNewArr.push(element[i]);
       }
       newArr.push(insideNewArr);
     } else {
       newArr.push(element);
     }
   }
+  return newArr;
+}
+
+
+function deeperCopy(nestedArr) {
+  let newArr = [];
+
+  for (let i = 0; i < nestedArr.length; i++) {
+    const element = nestedArr[i];
+
+    if (Array.isArray(element)) {
+      let insideNewArr = [];
+      for (let j = 0; j < element.length; j++) {
+        insideNewArr.push(element[j]);
+      }
+      newArr.push(insideNewArr);
+    } else {
+      newArr.push(element);
+    }
+  }
+
   return newArr;
 }
