5c32ad7616703aa177c36cfc985a59701e4364bd
Sanjeev Rai <87857906+22sanjeevrai22@users.noreply.github.com>
2025-07-11 10:04:52 +0545

    Update my-unshift.js
    New Functions

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -3,3 +3,18 @@ const myUnshift = (arr, value) => {
   const haha = arr.unshift(value);
   return arr;
 };
+
+//Next Method
+function myUnshift2(array,value){
+    let result=[]
+    result.push(value)
+    for (let i=0;i<array.length;i++){
+        result[i+1]=array[i]
+    }
+    return result
+}
+console.log(myUnshift([1, 2, 3], 0))
+
+function myUnshift3(arr, val) {
+  return [val].concat(arr);
+}
